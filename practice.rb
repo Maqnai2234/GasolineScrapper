ae3670975af54b540aef62e2a45f9860ecb17fc1
Alexei <alexeim763@gmail.com>
2017-06-20 00:54:45 -0500

    Add expresion regular for scrapping lat and lon in codes

--- practice.rb
+++ practice.rb
@@ -6,6 +6,7 @@ require 'mechanize'
 
 html = open("http://www.facilito.gob.pe/facilito/actions/MapaAction.do?departamento=210000&provincia=211100&distrito=211101&producto=70&method=mostrarMapa&subtitulocabecera=Precio%20de%20Gasolina%20y%20Diesel%20en%20Estaciones%3Cbr%3Ede%20Servicio%20y%20Grifos%20formales&subtitulocabecera2=Precio%20de%20Gasolina%20y%20Diesel%20en%20Estaciones%20de%20Servicio%20y%20Grifos%20formales&tipo=LIQ&codigoOSI=21354")
 doc = Nokogiri::HTML(html)
+a = doc.css('script')[6].scan(/\[.*\]/)[0].tr('\"','"').to_s
 puts doc
 
 Pry.start(binding)
